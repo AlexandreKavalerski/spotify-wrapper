6761f642f9e1b3269e2c56d2d57984405b926326
Alexandre <kavalerskialexandre@gmail.com>
2017-12-18 01:51:14 -0300

    Correcting fetch usage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
+
 export const search = (query, type) =>
-  fetch(`api.spotify.com/v1/search?q=${query}&type=${type}`)
-    .then(data => data.json())
-export const searchAlbums = () => { }
-export const searchArtists = () => { }
-export const searchTracks = () => { }
-export const searchPlaylists = () => { }
+  global.fetch(`api.spotify.com/v1/search?q=${query}&type=${type}`)
+    .then(data => data.json());
+export const searchAlbums = () => { };
+export const searchArtists = () => { };
+export const searchTracks = () => { };
+export const searchPlaylists = () => { };
