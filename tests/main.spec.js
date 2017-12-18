f7c2d812267ac369dc28ba22ccb17d3c4177bce1
Alexandre <kavalerskialexandre@gmail.com>
2017-12-18 00:35:03 -0300

    Update configs

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,31 +1,9 @@
-var expect = require('chai').expect;
+import { expect } from 'chai';
 
-describe('Main', function(){
-    var arr;
-
-    //roda uma vez antes de cada bloco
-    beforeEach(function(){
-        arr = [1, 2, 3];
-    });
-
-    //teste de tipos ou se existe:  smoke test
-    it('should be an array', () => {
-        expect(arr).to.be.an('array');
-    });
-
-    it('should have a size 4 when push another value to the array', function(){
-        arr.push(4);
-        expect(arr).to.have.lengthOf(4);
-        
-    });
-
-    it('should have a size of 2 when pop a value from the array', function(){
-        arr.pop();
-        expect(arr).to.have.lengthOf(2);
-    });
-
-    it('should remove the value 3 when use pop in the array', function(){
-        arr.pop();
-        expect(arr).to.not.include(3);
+describe('Spotify Wrapper', () => {
+    describe('smoke tests', () => {
+        it('should exist the search method', () => {
+            expect(search).to.exist;
+        });
     });
 });
