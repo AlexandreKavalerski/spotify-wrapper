39c356ea1d4862b097117fd804ac54015b99729d
Alexandre <kavalerskialexandre@gmail.com>
2017-12-21 14:42:24 -0300

    Correcting API url called method

--- tests/album.spec.js
+++ tests/album.spec.js
@@ -3,7 +3,6 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
 import { getAlbum, getAlbums, getAlbumTracks } from '../src/albums';
-import { API_URL } from '../src/config';
 
 /* eslint no-unused-expressions: 0 */
 sinonStubPromise(sinon);
@@ -42,10 +41,10 @@ describe('Album', () => {
 
     it('should call fetch with the correct URL', () => {
       getAlbum('0lw68yx3MhKflWFqCsGkIs');
-      expect(stubedFetch).to.be.calledWith(`${API_URL}/albums/0lw68yx3MhKflWFqCsGkIs`);
+      expect(stubedFetch).to.be.calledWith('https://api.spotify.com/v1/albums/0lw68yx3MhKflWFqCsGkIs');
 
       getAlbum('0lw68yx3MhKflWFqCsGkIk');
-      expect(stubedFetch).to.be.calledWith(`${API_URL}/albums/0lw68yx3MhKflWFqCsGkIk`);
+      expect(stubedFetch).to.be.calledWith('https://api.spotify.com/v1/albums/0lw68yx3MhKflWFqCsGkIk');
     });
 
     it('should return the correct data from Promise', () => {
@@ -68,10 +67,10 @@ describe('Album', () => {
 
     it('should call fetch with the correct URL', () => {
       getAlbums(albumsIds);
-      expect(stubedFetch).to.be.calledWith(`${API_URL}/albums?ids=0lw68yx3MhKflWFqCsGkIs,12Chz98pHFMPJEknJQMWvI,0eFHYz8NmK75zSplL5qlfM`);
+      expect(stubedFetch).to.be.calledWith('https://api.spotify.com/v1/albums?ids=0lw68yx3MhKflWFqCsGkIs,12Chz98pHFMPJEknJQMWvI,0eFHYz8NmK75zSplL5qlfM');
 
       getAlbums('0lw68yx3MhKflWFqCsGkIs');
-      expect(stubedFetch).to.be.calledWith(`${API_URL}/albums?ids=0lw68yx3MhKflWFqCsGkIs`);
+      expect(stubedFetch).to.be.calledWith('https://api.spotify.com/v1/albums?ids=0lw68yx3MhKflWFqCsGkIs');
     });
 
     it('should return the correct data from Promise', () => {
@@ -90,7 +89,7 @@ describe('Album', () => {
 
     it('should call fetch with the correct URL', () => {
       getAlbumTracks('0lw68yx3MhKflWFqCsGkIs');
-      expect(stubedFetch).to.be.calledWith(`${API_URL}/albums/0lw68yx3MhKflWFqCsGkIs/tracks`);
+      expect(stubedFetch).to.be.calledWith('https://api.spotify.com/v1/albums/0lw68yx3MhKflWFqCsGkIs/tracks');
     });
 
     it('should return the correct data from Promise', () => {
