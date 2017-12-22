81b15c268def746151ea6c1c375e196d156b06d9
Alexandre <kavalerskialexandre@gmail.com>
2017-12-22 17:26:14 -0300

    Refactoring library to use class/constructor

--- src/index.js
+++ src/index.js
@@ -17,15 +17,23 @@ import {
   getTopTracksFromArtist,
 } from './artists';
 
-module.exports = {
-  search,
-  searchAlbums,
-  searchArtists,
-  searchPlaylists,
-  searchTracks,
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-  getArtist,
-  getTopTracksFromArtist,
-};
+import { API_URL } from './config';
+
+// module.exports = {
+//   search,
+//   searchAlbums,
+//   searchArtists,
+//   searchPlaylists,
+//   searchTracks,
+//   getAlbum,
+//   getAlbums,
+//   getAlbumTracks,
+//   getArtist,
+//   getTopTracksFromArtist,
+// };
+export default class SpotifyWrapper {
+  constructor(options) {
+    this.apiURL = options.apiURL || API_URL;
+    this.token = options.token;
+  }
+}
